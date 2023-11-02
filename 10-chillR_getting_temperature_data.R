a841f8c3d0d34e4f26969d98de65a16cbf2c70a4
eikeluedeling <luedeling@uni-bonn.de>
2023-11-02 17:18:36 +0100

    02 Nov

--- 10-chillR_getting_temperature_data.R
+++ 10-chillR_getting_temperature_data.R
@@ -3,6 +3,10 @@ station_list<-handle_gsod(action="list_stations",
                          location=c(7.10,50.73),
                          time_interval=c(1990,2020))
 
+station_list_Oman<-handle_gsod(action="list_stations",
+                          location=c(long=57.66,lat=23.07),
+                          time_interval=c(1990,2020))
+
 # station_list<-read.csv("data/station_list.csv")
 # weather<-list(`KOLN BONN`=read.csv("data/Bonn_weather.csv"))
 # cleaned_weather<-list(`KOLN BONN`=read.csv("data/Bonn_chillR_weather.csv"))
@@ -21,8 +25,14 @@ cleaned_weather<-handle_gsod(weather)
 
 cleaned_weather[[1]][1:20,]
 
-
+dir.create("data")
 write.csv(station_list,"data/station_list.csv",row.names=FALSE)
 write.csv(weather[[1]],"data/Bonn_weather.csv",row.names=FALSE)
 write.csv(cleaned_weather[[1]],"data/Bonn_chillR_weather.csv",row.names=FALSE)
  
+?handle_dwd
+
+handle_dwd("list_stations",
+           location=c(7.10,50.73))
+
+
